e4eec62609707e0dc77ea569b65ae246561ebc63
Jiwon Jung <jjw2150@naver.com>
2018-05-31 23:28:07 +0900

    toDoList 화면 만듦

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,61 +11,99 @@ import {
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
+  loadToDoList() {
+    const toDoList = [
+      {
+        description:"eat",
+        completed:false,
+      },
+      {
+        description:"김대훈패기",
+        completed:true,
+      },
+      {
+        description:"과제",
+        completed:false,
+      },
+      {
+        description:"하기싫다",
+        completed:true,
+      },
+      {
+        description:"내일발표",
+        completed:true,
+      },
+      {
+        description:"eat",
+        completed:false,
+      },
+      {
+        description:"김대훈패기",
+        completed:true,
+      },
+      {
+        description:"과제",
+        completed:false,
+      },
+      {
+        description:"하기싫다",
+        completed:true,
+      },
+      {
+        description:"내일발표",
+        completed:true,
+      },
+      {
+        description:"eat",
+        completed:false,
+      },
+      {
+        description:"김대훈패기",
+        completed:true,
+      },
+      {
+        description:"과제",
+        completed:false,
+      },
+      {
+        description:"하기싫다",
+        completed:true,
+      },
+      {
+        description:"내일발표",
+        completed:true,
+      },
+    ];
+    
+    return toDoList;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-
-          <View style={styles.welcomeView}>
-            <Text>
-              지원이의 첫앱 환영해용
-            </Text>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
+        <View style={styles.header}>
+          <Text style={styles.headerTitleText}>
+            JIWON TO DO
+          </Text>
         </View>
+        <FlatList
+          data={this.loadToDoList()}
+          renderItem={
+            ({item}) =>
+              <View style={styles.toDoListItem}>
+                <Text style={styles.toDoListItemText}>
+                  {item.description}
+                </Text>
+              </View>
+          }
+        />
       </View>
     );
   }
@@ -118,6 +156,24 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 30,
+  },
+  toDoListItem: {
+    padding: 20,
+  },
+  toDoListItemText: {
+    fontSize: 20,  
+  },
+  header: {
+    height: 62,
+    backgroundColor: '#e1a7f9',
+    paddingTop: 12,
+    justifyContent: 'center',    
+    alignItems: 'center',
+  },
+  headerTitleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+
   },
   welcomeContainer: {
     alignItems: 'center',
