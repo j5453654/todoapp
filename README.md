7306397790c1b6a49dbe36bab7d5a20c6d488b9b
Jiwon Jung <jjw2150@naver.com>
2018-06-02 16:01:56 +0900

    할 일 추가 팝업창 디자인

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # todoapp
 
 Jiwon Jung
+
+# Reference
+* expo: https://docs.expo.io/versions/v27.0.0/
+* react-native: https://facebook.github.io/react-native
+* eslint 설치: https://www.youtube.com/watch?v=yDZuJD9vXE
+* color picker: https://www.google.co.kr/search?rlz=1C1OKWM_koKR783KR784&ei=qTUSW6lKxpTzBcLEuYAK&q=color+picker&oq=color+picker&gs_l=psy-ab.3..0l10.2244.3617.0.3720.7.5.0.2.2.0.362.528.0j1j0j1.2.0....0...1c.1.64.psy-ab..3.4.533...0i67k1.0.kSjVXyevz5w
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,106 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
+  FlatList,
+  Modal,
+  TextInput,
 } from 'react-native';
-import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
-import { FlatList } from 'react-native-gesture-handler';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  toDoListItem: {
+    padding: 20,
+  },
+  toDoListItemText: {
+    fontSize: 20,
+  },
+  header: {
+    height: 62,
+    backgroundColor: '#e1a7f9',
+    paddingTop: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerTitleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  plus: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#f2e3f7',
+    height: 62,
+    width: 62,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 62 / 2,
+  },
+  plusFont: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  modalView: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modal: {
+    backgroundColor: 'white',
+    // height: 250,
+    width: 350,
+  },
+  okButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 12,
+    paddingBottom: 12,
+    borderWidth: 1,
+    flex: 1,
+  },
+  okText: {
+    fontSize: 20,
+  },
+  buttonView: {
+    flexDirection: 'row',
+  },
+  modalTitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 12,
+    marginBottom: 12,
+  },
+  modalTitleView: {
+    backgroundColor: '#f2e3f7',
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    margin: 10,
+  },
+});
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+  state = {
+    modalVisible: false,
+    text: '',
+  };
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
 
   loadToDoList() {
     const toDoList = [
@@ -41,197 +125,86 @@ export default class HomeScreen extends React.Component {
         completed:true,
       },
     ];
-    
     return toDoList;
   }
 
   render() {
     return (
       <View style={styles.container}>
-    
         <View style={styles.header}>
           <Text style={styles.headerTitleText}>
             JIWON TO DO
           </Text>
         </View>
-        
         <FlatList
           data={this.loadToDoList()}
           renderItem={
-            ({item}) =>
+            ({ item }) => (
               <View style={styles.toDoListItem}>
                 <Text style={styles.toDoListItemText}>
                   {item.description}
                 </Text>
               </View>
+            )
           }
         />
-        
-        <TouchableOpacity style={styles.plus}>
+
+        <TouchableOpacity
+          style={styles.plus}
+          onPress={() => {
+                    this.setModalVisible(true);
+                  }}
+        >
           <Text style={styles.plusFont}>
             +
           </Text>
         </TouchableOpacity>
+
+        <Modal
+          animationType="slide"
+          transparent
+          visible={this.state.modalVisible}
+        >
+          <View style={styles.modalView}>
+            <View style={styles.modal}>
+              <View style={styles.modalTitleView}>
+                <Text style={styles.modalTitle}>
+                할 일 추가
+                </Text>
+              </View>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={text => this.setState({ text })}
+                value={this.state.text}
+              />
+              <View style={styles.buttonView}>
+                <TouchableOpacity
+                  style={styles.okButton}
+                  onPress={() => {
+                            this.setModalVisible(!this.state.modalVisible);
+                          }}
+                >
+                  <Text style={styles.okText}>
+                    확인
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.okButton}
+                  onPress={() => {
+                            this.setModalVisible(!this.state.modalVisible);
+                          }}
+                >
+                  <Text style={styles.okText}>
+                    취소
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  toDoListItem: {
-    padding: 20,
-  },
-  toDoListItemText: {
-    fontSize: 20,  
-  },
-  header: {
-    height: 62,
-    backgroundColor: '#e1a7f9',
-    paddingTop: 12,
-    justifyContent: 'center',    
-    alignItems: 'center',
-  },
-  headerTitleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  plus: {
-    position: 'absolute', 
-    bottom: 20,
-    right: 20,
-    backgroundColor: '#f2e3f7',
-    height: 62,
-    width: 62,
-    justifyContent: 'center',    
-    alignItems: 'center',
-    borderRadius: 62/2,
-  },
-  plusFont: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-   welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-  welcomeView: {
-    alignItems: 'center',
-    backgroundColor: 'gray',
-  },
-});
